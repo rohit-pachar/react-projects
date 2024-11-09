6dc05580348eee8768ee278e3d4770bd820d3a56
rohit-pachar <rohitpachar05@gmail.com>
2024-11-10 04:37:35 +0530

    starting to make custom react

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,14 @@
+import Youtube from "./Youtube"
+
+function App() {
+
+  return (
+    <>
+     <h1>Main React App</h1>
+      <Youtube />
+    </>
+   
+  )
+}
+
+export default App
---
+++ 01basicvite/src/Youtube.jsx
@@ -0,0 +1,9 @@
+function Youtube(){
+
+
+    return (
+        <h1>Youtube app</h1>
+    )
+}
+
+export default Youtube
